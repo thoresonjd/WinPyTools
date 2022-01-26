45e7c80f2f25ed23b405d978509ba24e4c500126
Justin Thoreson <thoresonjd@gmail.com>
2022-01-26 12:22:58 -0800

    Created separate file for temp func1

--- WinPyTools.py
+++ WinPyTools.py
@@ -8,17 +8,14 @@ https://nitratine.net/blog/post/how-to-make-hotkeys-in-python/
 
 # Import modules
 from pynput.keyboard import Key, KeyCode, Listener
-
-# Functions to execute
-def func1():
-    print('func1')
+import func1
 
 def func2():
     print('func2')
 
 # Map functions to hotkey combinations
 COMBINATIONS = {
-    frozenset([Key.shift, KeyCode(vk=ord('D'))]): func1,
+    frozenset([Key.shift, KeyCode(vk=ord('D'))]): func1.func1,
     frozenset([Key.shift, KeyCode(vk=ord('L'))]): func2
 }
 
